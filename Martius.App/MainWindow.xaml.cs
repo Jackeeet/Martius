50ad1db4913ddb27f8b04657ff2cdbb9437cf2e5
Polina Grabar <polina.grabar@gmail.com>
2021-05-10 09:22:22 +0400

    initial commit

---
+++ Martius.App/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Martius.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
